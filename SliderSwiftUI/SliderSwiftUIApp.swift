ec9bd5eecb5af9a4a8d8d07d64e2d776b4679bea
artemyderik <derry0808@gmail.com>
2023-11-14 15:45:18 +0700

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Артемий Дериглазов on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
