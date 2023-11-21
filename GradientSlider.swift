9e3d3f6c631bcf73ac6dcb830c59ed25b87c8890
artemyderik <derry0808@gmail.com>
2023-11-21 09:23:59 +0700

    It works.

---
+++ GradientSlider.swift
@@ -0,0 +1,58 @@
+//
+//  GradientSlider.swift
+//  SliderSwiftUI
+//
+//  Created by Артемий Дериглазов on 19.11.2023.
+//
+
+import SwiftUI
+
+struct GradientSlider: View {
+    @Binding var value: Double
+    let gradientColors: [Color]
+    let chosenColor: ChosenColor
+    
+    enum ChosenColor {
+        case red, green, blue
+    }
+    
+    var body: some View {
+         
+        HStack {
+            
+            Text(textByColor())
+                .frame(width: 50)
+            
+            ZStack(alignment: .leading) {
+                            LinearGradient(
+                                gradient: Gradient(colors: gradientColors),
+                                startPoint: .leading,
+                                endPoint: .trailing)
+                                .frame(width: 190, height: 4)
+                                .cornerRadius(5)
+                            
+                            Slider(value: $value, in: 0...255, step: 1)
+                                .accentColor(Color.clear)
+                                .frame(width: 190)
+                        }
+
+            TextField("", value: $value, formatter: NumberFormatter())
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .frame(width: 50)
+                .padding()
+        }
+    }
+    
+    private func textByColor() -> String {
+        switch chosenColor {
+        case .red:
+            return "R"
+        case .green:
+            return "G"
+        case .blue:
+            return "B"
+        }
+    }
+}
+
--- SliderSwiftUI/ContentView.swift
+++ SliderSwiftUI/ContentView.swift
@@ -8,12 +8,71 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double.random(in: 0...255)
+    @State private var greenSliderValue = Double.random(in: 0...255)
+    @State private var blueSliderValue = Double.random(in: 0...255)
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Rectangle()
+                .frame(height: UIScreen.main.bounds.height / 3.5)
+                .cornerRadius(50)
+                .foregroundColor(
+                    Color(
+                        red: redSliderValue / 255,
+                        green: greenSliderValue / 255,
+                        blue: blueSliderValue / 255)
+                )
+            VStack {
+                GradientSlider(
+                    value: $redSliderValue,
+                    gradientColors: [
+                        Color(red: 0, green: greenSliderValue / 255, blue: blueSliderValue / 255),
+                        Color(red: 1, green: greenSliderValue / 255, blue: blueSliderValue / 255)
+                    ], chosenColor: .red)
+                
+                
+                GradientSlider(
+                    value: $greenSliderValue,
+                    gradientColors: [
+                        Color(red: redSliderValue / 255, green: 0, blue: blueSliderValue / 255),
+                        Color( red: redSliderValue / 255, green: 1, blue: blueSliderValue / 255)
+                    ], chosenColor: .green)
+                
+                GradientSlider(
+                    value: $blueSliderValue,
+                    gradientColors: [
+                        Color(red: redSliderValue / 255, green: greenSliderValue / 255,blue: 0),
+                        Color(red: redSliderValue / 255,green: greenSliderValue / 255,blue: 1)
+                    ], chosenColor: .blue)
+            }
+                .toolbar {
+                        ToolbarItemGroup(placement: .keyboard) {
+                            HStack {
+                                Button(action: {
+                                    print("Up arrow")
+                                }) {
+                                    Image(systemName: "chevron.compact.up")
+                                }
+                                .padding()
+
+                                Button(action: {
+                                    print("Down arrow")
+                                }) {
+                                    Image(systemName: "chevron.compact.down")
+                                }
+                                
+                                Spacer()
+                                
+                            Button("Done") {
+                                UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                                print("Clicked")
+                            }
+                        }
+                    }
+                }
+            
+            Spacer()
         }
         .padding()
     }
