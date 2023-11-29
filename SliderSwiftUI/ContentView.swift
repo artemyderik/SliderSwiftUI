7c64dc71a5034e9b56b6f29dd684bb1a6f1150fe
artemyderik <derry0808@gmail.com>
2023-11-29 18:22:49 +0700

    Fixed bug with hiding keyboard every time you tapping the up or down tabbarItem's button.

--- SliderSwiftUI/ContentView.swift
+++ SliderSwiftUI/ContentView.swift
@@ -12,135 +12,135 @@ struct ContentView: View {
     @State private var greenSliderValue = Double.random(in: 0...255)
     @State private var blueSliderValue = Double.random(in: 0...255)
     
-    @FocusState private var isRedFieldFocused: Bool
-    @FocusState private var isGreenFieldFocused: Bool
-    @FocusState private var isBlueFieldFocused: Bool
+    @FocusState private var focusedField: Field?
 
+    enum Field {
+        case red, green, blue
+    }
+    
     var body: some View {
-        VStack {
-            ColorisedRectangle(
-                redSliderValue: redSliderValue,
-                greenSliderValue: greenSliderValue,
-                blueSliderValue: blueSliderValue
-            )
-            
+        ZStack {
+            Color(.white)
+                .ignoresSafeArea()
+                .onTapGesture {
+                    focusedField = nil
+                }
             VStack {
-                GradientSlider(
-                    value: $redSliderValue,
-                    gradientColors: [
-                        Color(
-                            red: 0,
-                            green: greenSliderValue / 255,
-                            blue: blueSliderValue / 255
-                        ),
-                        Color(
-                            red: 1,
-                            green: greenSliderValue / 255,
-                            blue: blueSliderValue / 255
-                        )
-                    ],
-                    chosenColor: .red
+                ColorisedRectangle(
+                    redSliderValue: redSliderValue,
+                    greenSliderValue: greenSliderValue,
+                    blueSliderValue: blueSliderValue
                 )
-                .onTapGesture {
-                    isRedFieldFocused = true
-                    isGreenFieldFocused = false
-                    isBlueFieldFocused = false
-                }
-                .focused($isRedFieldFocused)
                 
-                GradientSlider(
-                    value: $greenSliderValue,
-                    gradientColors: [
-                        Color(
-                            red: redSliderValue / 255,
-                            green: 0,
-                            blue: blueSliderValue / 255
-                        ),
-                        Color(
-                            red: redSliderValue / 255,
-                            green: 1,
-                            blue: blueSliderValue / 255
-                        )
-                    ],
-                    chosenColor: .green
-                )
-                .onTapGesture {
-                    isRedFieldFocused = false
-                    isGreenFieldFocused = true
-                    isBlueFieldFocused = false
+                VStack {
+                    GradientSlider(
+                        value: $redSliderValue,
+                        gradientColors: [
+                            Color(
+                                red: 0,
+                                green: greenSliderValue / 255,
+                                blue: blueSliderValue / 255
+                            ),
+                            Color(
+                                red: 1,
+                                green: greenSliderValue / 255,
+                                blue: blueSliderValue / 255
+                            )
+                        ],
+                        chosenColor: .red
+                    )
+                    .focused($focusedField, equals: .red )
+                    
+                    GradientSlider(
+                        value: $greenSliderValue,
+                        gradientColors: [
+                            Color(
+                                red: redSliderValue / 255,
+                                green: 0,
+                                blue: blueSliderValue / 255
+                            ),
+                            Color(
+                                red: redSliderValue / 255,
+                                green: 1,
+                                blue: blueSliderValue / 255
+                            )
+                        ],
+                        chosenColor: .green
+                    )
+                    .focused($focusedField, equals: .green )
+                    
+                    GradientSlider(
+                        value: $blueSliderValue,
+                        gradientColors: [
+                            Color(
+                                red: redSliderValue / 255,
+                                green: greenSliderValue / 255,
+                                blue: 0
+                            ),
+                            Color(
+                                red: redSliderValue / 255,
+                                green: greenSliderValue / 255,
+                                blue: 1
+                            )
+                        ],
+                        chosenColor: .blue
+                    )
+                    .focused($focusedField, equals: .blue )
+                    
                 }
-                .focused($isGreenFieldFocused)
                 
-                GradientSlider(
-                    value: $blueSliderValue,
-                    gradientColors: [
-                        Color(
-                            red: redSliderValue / 255,
-                            green: greenSliderValue / 255,
-                            blue: 0
-                        ),
-                        Color(
-                            red: redSliderValue / 255,
-                            green: greenSliderValue / 255,
-                            blue: 1
-                        )
-                    ],
-                    chosenColor: .blue
-                )
-                .onTapGesture {
-                    isRedFieldFocused = false
-                    isGreenFieldFocused = false
-                    isBlueFieldFocused = true
-                }
-                .focused($isBlueFieldFocused)
-                
-            }
-            
-            .toolbar {
-                ToolbarItemGroup(placement: .keyboard) {
-                    HStack {
-                        Button(action: upButtonTapped) {
-                            Image(systemName: "chevron.compact.up")
-                        }
-                        .padding()
-                        
-                        Button(action: downButtonTapped) {
-                            Image(systemName: "chevron.compact.down")
-                        }
-                        
-                        Spacer()
-                        
-                        Button("Done") {
-                            UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-                            print("Clicked")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        HStack {
+                            Button(action: upButtonTapped) {
+                                Image(systemName: "chevron.compact.up")
+                            }
+                            .padding()
+                            
+                            Button(action: downButtonTapped) {
+                                Image(systemName: "chevron.compact.down")
+                            }
+                            
+                            Spacer()
+                            
+                            Button("Done") {
+                                focusedField = nil
+                                print("Clicked")
+                            }
                         }
                     }
                 }
+                
+                Spacer()
             }
             
-            Spacer()
+            .padding()
         }
-        
-        .padding()
     }
     
     private func upButtonTapped() {
-        if isBlueFieldFocused {
-            isBlueFieldFocused = false
-            isGreenFieldFocused = true
-        } else {
-            isGreenFieldFocused = false
-            isRedFieldFocused = true
+        switch focusedField {
+        case .blue:
+            focusedField = .green
+        case .green:
+            focusedField = .red
+        case .red:
+            focusedField = .blue
+        case .none:
+            focusedField = nil
         }
     }
 
     private func downButtonTapped() {
-        if isRedFieldFocused {
-            isRedFieldFocused = false
-            isGreenFieldFocused = true
-        } else {
-            isGreenFieldFocused = false
-            isBlueFieldFocused = true
+        switch focusedField {
+        case .red:
+            focusedField = .green
+        case .green:
+            focusedField = .blue
+        case .blue:
+            focusedField = .red
+        case .none:
+            focusedField = nil
         }
     }
 }
